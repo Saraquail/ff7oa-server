5f68fb36792da2bd4851ebfdfba2cbf68217c15b
Sara R Mills <SaraRMills@gmail.com>
2020-04-17 15:41:33 -0400

    changed db to database for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://saraquail@localhost/ff7oa',
-  JWT_SECRET: process.env.JWT_SECRET || '@5oi2h4o2380s9#56adiw'
+  DATABASE_URL: process.env.DB_URL || 'postgresql://saraquail@localhost/ff7oa',
+  JWT_SECRET: process.env.JWT_SECRET || '@5oi2h4o2380s9#56adiw',
+  TEST_DATABASE_URL: process.env.DB_URL || 'postgresql://saraquail@localhost/ff7oa-test',
+
 }
