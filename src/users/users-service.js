b21ea6c5f6f7e1dc65a022fcff9bf3df581ac1fa
Sara R Mills <SaraRMills@gmail.com>
2020-04-20 10:32:14 -0400

    relaxed password requirements

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,12 +1,12 @@
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+const REGEX_UPPER_LOWER_NUMBER = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])+/
 
 const xss = require('xss')
 const bcrypt = require('bcryptjs')
 
 const UsersService = {
   validatePassword(password) {
-    if (password.length < 8) {
-      return 'Password must be longer than 8 characters'
+    if (password.length < 6) {
+      return 'Password must be longer than 6 characters'
     }
     if (password.length > 72) {
       return 'Password must be less than 72 characters'
@@ -14,8 +14,8 @@ const UsersService = {
     if (password.startsWith(' ') || password.endsWith(' ')) {
       return 'Password must not start or end with empty spaces'
     }
-    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return 'Password must contain 1 upper case, lower case, number and special character'
+    if (!REGEX_UPPER_LOWER_NUMBER.test(password)) {
+      return 'Password must contain 1 upper case, lower case, and number'
     }
     return null
   },
